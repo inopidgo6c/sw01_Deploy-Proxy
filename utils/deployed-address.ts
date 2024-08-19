e269ae1c0a2c41040f4e284ad7d8019d065d79d2
inopidgo6c d1 <puviodei_dao4@outlook.com>
2024-08-19 17:10:38 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x56AF1C0d733Ad95e93c997ccb0Ebe2dff512840b'
+export const ProxyAdmin = '0xEdACeA803224310D445E2c2cC71a66c90086cC77'
+export const SWTRImplementation = '0x67d223190A9447BfC372Ac8137e18819FACF1653'
     
